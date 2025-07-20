e209705c79c0a8736ad1da265a81f2752ab55e4b
Greivin Arguedas <greivin.arguegud@gmail.com>
2025-07-19 20:28:38 -0600

    Creacion de paquetes y Clases Limpias
    Se crea toda la estructura inicial con el fin de ahorrar trabajo al crear carpetas y clases, solo queda programarlas

---
+++ src/Subsistemas/Alarmas/ControlAlarma.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Subsistemas.Alarmas;
+
+/**
+ *
+ * @author UTN
+ */
+public class ControlAlarma {
+    
+}
