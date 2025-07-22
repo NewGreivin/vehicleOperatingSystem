1c096a71c3263d6fcec2738050ff5fc559f4aa09
Greivin Arguedas <greivin.arguegud@gmail.com>
2025-07-21 21:58:42 -0600

    Creacion de clase motor
    Clase que simula el motor de un vehiculo el cual contiene atributos encargados de controlar el encendido, revoluciones y potencia del motor

--- src/Core/Vehiculos/Motor.java
+++ src/Core/Vehiculos/Motor.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * @author Greivin
  */
 package Core.Vehiculos;
 
 /**
- *
- * @author UTN
+ * Clase que simula el motor del vehiculo
+ * Representa lo esencial para el funcionamiento
+ * controla el encendido, revoluciones y potencia del motor
  */
+
 public class Motor {
+    private boolean encendido;
+    private int revoluciones;  // en RPM
+    private int potencia;         // en caballos de fuerza (HP)
+
+    public boolean isEncendido() {
+        return encendido;
+    }
+    public int getRevoluciones() {
+        return revoluciones;
+    }
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setRevoluciones(int revoluciones) {
+        this.revoluciones = revoluciones;
+    }
+
+    public Motor(int potencia) {
+        this.encendido = false;
+        this.revoluciones = 0;
+        this.potencia = potencia;
+    }
     
+    public void encender(){
+        this.encendido = true;
+    }
+            
+    public void apagar() {
+        this.encendido = false;
+        revoluciones = 0;
+    }
 }
