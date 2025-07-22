9cd9d1d26efb998aa1097ff5d74337c7d1e8db99
Greivin Arguedas <greivin.arguegud@gmail.com>
2025-07-21 20:44:45 -0600

    Clase Subsistema (Interfaz)
    Almacena los metodos de comportamientos COMUNES necesarios para todos los subsitemas existentes del vehiculo con esta clase interfaz

--- src/Subsistemas/Subsistema.java
+++ src/Subsistemas/Subsistema.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+/**
+ * @author Greivin
  */
 package Subsistemas;
 
-/**
- *
- * @author UTN
- */
-public interface Subsistema {
-    
+public interface Subsistema<T> {
+    public void encender (T t);
+    public void apagar (T t);
+    public String estadoActual();
 }
