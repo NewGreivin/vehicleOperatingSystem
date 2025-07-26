3a2cae0fb7c378fe31a9ca7d3fa0a62133637718
GenesisJimenezZZ <gejimenezfl@est.utn.ac.cr>
2025-07-25 19:33:59 -0600

    Se termino el paquete encendido
    Enum, metodos y atributos

--- src/Subsistemas/Encendido/EstadoEncendido.java
+++ src/Subsistemas/Encendido/EstadoEncendido.java
@@ -1,13 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
 package Subsistemas.Encendido;
 
 /**
  *
- * @author UTN
+ * @author Genesis
  */
-public class EstadoEncendido {
-    
+public enum EstadoEncendido {
+    ENCENDIDO,
+    APAGADO
 }
--- src/Subsistemas/Encendido/SistemaEncendido.java
+++ src/Subsistemas/Encendido/SistemaEncendido.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Subsistemas.Encendido;
-
 /**
  *
- * @author UTN
+ * @author Genesis
  */
-public class SistemaEncendido {
-    
+
+
+public class SistemaEncendido implements Subsitema {
+    private EstadoEncendido estado;
+
+    public SistemaEncendido() {
+        this.estado = EstadoEncendido.APAGADO;
+    }
+
+    @Override
+    public void encender() {
+        estado = EstadoEncendido.ENCENDIDO;
+        System.out.println("El vehículo ha sido encendido.");
+    }
+
+    @Override
+    public void apagar() {
+        estado = EstadoEncendido.APAGADO;
+        System.out.println("El vehículo ha sido apagado.");
+    }
+
+    @Override
+    public String estadoActual() {
+        return estado.name();
+    }
+
+    public boolean estaEncendido() {
+        return estado == EstadoEncendido.ENCENDIDO;
+    }
 }
