7f1b8d77993ec6c337594ed76ceeba70bb7826c4
GenesisJimenezZZ <gejimenezfl@est.utn.ac.cr>
2025-07-26 21:42:35 -0600

    Se resubio encedido
    todo igual

--- src/Subsistemas/Encendido/SistemaEncendido.java
+++ src/Subsistemas/Encendido/SistemaEncendido.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Subsistemas.Encendido;
-
+import Core.Vehiculos.Motor;
 /**
  *
- * @author UTN
+ * @author Genesis
  */
 public class SistemaEncendido {
-    
+
+    private EstadoEncendido estado;
+    private final Motor motor;
+
+    public SistemaEncendido(Motor motor) {
+        this.motor = motor;
+        this.estado = EstadoEncendido.APAGADO;
+    }
+
+    public void alternarEncendido() {
+        if (estado == EstadoEncendido.APAGADO) {
+            motor.encender();
+            estado = EstadoEncendido.ENCENDIDO;
+        } else {
+            motor.apagar();
+            estado = EstadoEncendido.APAGADO;
+        }
+    }
+
+    public EstadoEncendido getEstado() {
+        return estado;
+    }
 }
