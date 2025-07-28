806ab3457bc337c324de37d9677420ea0cabd937
GenesisJimenezZZ <gejimenezfl@est.utn.ac.cr>
2025-07-27 21:02:57 -0600

    Se modifico sistema de sensores

--- src/Subsistemas/Sensores/SistemaSensores.java
+++ src/Subsistemas/Sensores/SistemaSensores.java
@@ -4,6 +4,15 @@
  */
 package Subsistemas.Sensores;
 import Interfaces.Activable;
+import Subsistemas.Encendido.SistemaEncendido;
+import Subsistemas.Transmicion.PosicionTransmision;
+import Subsistemas.Transmicion.SistemaTransmicion;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
 
 /**
  *
@@ -11,21 +20,72 @@ import Interfaces.Activable;
  */
 public class SistemaSensores implements Activable {
     private boolean activos;
+    private SensorFrenoMano frenoMano;
+    private SensorReversa reversa;
+    private SistemaEncendido sistemaEncendido;
+    private SistemaTransmicion transmision;
 
-    public SistemaSensores() {
-        this.activos = false;
+    public SensorFrenoMano getFrenoMano() {
+        return frenoMano;
     }
-
-    @Override
+    public SensorReversa getReversa() {
+        return reversa;
+    } 
+    
+    public SistemaSensores(SistemaEncendido sistemaEncendido, SistemaTransmicion transmision) {
+        this.activos = false;
+        this.frenoMano = new SensorFrenoMano();
+        this.reversa = new SensorReversa();
+        this.sistemaEncendido = sistemaEncendido;
+        this.transmision = transmision;
+    }
+@Override
     public void encender() {
-        activos = true;
-        System.out.println("Sensores activados.");
+        if (sistemaEncendido.getEstado().isEncendido()) {
+            activos = true;
+            frenoMano.ponerFreno();
+        } else {
+            System.out.println("No se puede activar sensores: vehículo apagado.");
+        }
     }
 
     @Override
     public void apagar() {
         activos = false;
-        System.out.println("Sensores desactivados.");
+        reversa.desactivar();
+    }
+
+    public void verificar() {
+        if (!activos || !sistemaEncendido.getEstado().isEncendido()) {
+            return;
+        }
+// Activar sensor de reversa si está en reversa
+        if (transmision.getPosicion() == PosicionTransmision.REVERSE) {
+            reversa.activar();
+        } else {
+            reversa.desactivar();
+        }
+        
+        // Alerta si hay obstáculo al retroceder
+        if (reversa.isActivado() && reversa.isObstaculoDetectado()) {
+            reproducirAlerta();
+        }
+
+        // Alerta si se intenta mover con el freno de mano puesto
+        if (transmision.getVelocidad() > 0 && frenoMano.isActivado()) {
+            reproducirAlerta();
+        }
+    }
+private static void reproducirAlerta() {
+        try {
+            File sonido = new File("src/sonidos/alerta_sensor.wav");
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(sonido);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Error al reproducir sonido: " + e.getMessage());
+        }
     }
 
     public String estadoActual() {
