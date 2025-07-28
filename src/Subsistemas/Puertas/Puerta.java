b8d70f2560af34c39641dd0814af5724a8463587
Ricardo <richavesca@est.utn.ac.cr>
2025-07-27 20:08:07 -0600

    Se arreglaron errores
    Se arreglaron algunas cosas y se creo la variable bloqueada junto con sus metodos

--- src/Subsistemas/Puertas/Puerta.java
+++ src/Subsistemas/Puertas/Puerta.java
@@ -9,27 +9,42 @@ package Subsistemas.Puertas;
  */
 public class Puerta {
     private boolean puerta; //True: Abierta & False: Cerrada
-
+    private boolean bloqueada; // True: con seguro
+    
     public boolean isPuerta() {
         return puerta;
+    }
+    public boolean isBloqueada() {
+        return bloqueada;
     }
 
     public Puerta(boolean puerta) {
         this.puerta = puerta;
+        this.bloqueada = false;
     }
 
     @Override
     public String toString() {
-        return "Puerta{" + "puerta=" + puerta + '}';
+        return"Puerta{" + "puerta=" + puerta + ", bloqueada=" + bloqueada;
     }
     
     //metodos
     
     public void abrirPuerta() {
-        this.puerta = true;
+        if (!bloqueada) {
+            this.puerta = true;
+        } else {
+            System.out.println("Puerta bloqueada. No se puede abrir.");
+        }
     }
     public void cerrarPuerta() {
         this.puerta = false;
     }
-
+    
+    public void bloquear() {
+        this.bloqueada = true;
+    }
+    public void desbloquear() {
+        this.bloqueada = false;
+    }
 }
