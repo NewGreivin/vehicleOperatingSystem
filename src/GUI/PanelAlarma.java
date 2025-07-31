ccd8e2aea353b9dc3b2aa041db158535b29411f3
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-07-30 23:05:07 -0600

    Se edita el PanelAlarma
    Se hacen mejorias y se cambian los controles de lugar

--- src/GUI/PanelAlarma.java
+++ src/GUI/PanelAlarma.java
@@ -31,107 +31,172 @@ public class PanelAlarma extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        btnAlarma = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
-        btnCierre = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        lblEstadoBloqueo = new javax.swing.JLabel();
+        btnBloquear = new javax.swing.JButton();
+        btnDesbloquear = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        lblEstadoPuerta = new javax.swing.JLabel();
+        btnAbrir = new javax.swing.JButton();
+        btnCerrar = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        lblEstadoAlarma = new javax.swing.JLabel();
 
-        btnAlarma.setFont(new java.awt.Font("Elephant", 0, 14)); // NOI18N
-        btnAlarma.setText("Encender alarma");
-        btnAlarma.addActionListener(new java.awt.event.ActionListener() {
+        jLabel1.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setText("Bloqueo de puerta");
+
+        lblEstadoBloqueo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblEstadoBloqueo.setText("Estado: Desbloqueada");
+
+        btnBloquear.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnBloquear.setText("Bloquear");
+        btnBloquear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAlarmaActionPerformed(evt);
+                btnBloquearActionPerformed(evt);
             }
         });
 
-        btnCierre.setFont(new java.awt.Font("Elephant", 0, 14)); // NOI18N
-        btnCierre.setText("Cerrar cierre");
-        btnCierre.addActionListener(new java.awt.event.ActionListener() {
+        btnDesbloquear.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnDesbloquear.setText("Desbloquear");
+        btnDesbloquear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCierreActionPerformed(evt);
+                btnDesbloquearActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(btnCierre)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(41, Short.MAX_VALUE)
-                .addComponent(btnCierre)
-                .addGap(36, 36, 36))
-        );
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel2.setText("Puerta");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnAlarma, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(btnAlarma)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
+        lblEstadoPuerta.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblEstadoPuerta.setText("Estado: Cerrada");
+
+        btnAbrir.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnAbrir.setText("Abrir");
+        btnAbrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAbrirActionPerformed(evt);
+            }
+        });
+
+        btnCerrar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnCerrar.setText("Cerrar");
+        btnCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCerrarActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setBackground(new java.awt.Color(255, 51, 51));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel3.setText("Alarma");
+
+        lblEstadoAlarma.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblEstadoAlarma.setText("Estado: Inactiva");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(69, 69, 69)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addGap(32, 32, 32))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(lblEstadoBloqueo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(lblEstadoPuerta)
+                        .addGap(8, 8, 8)))
+                .addGap(50, 50, 50))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(btnBloquear)
+                .addGap(18, 18, 18)
+                .addComponent(btnDesbloquear)
+                .addGap(91, 91, 91)
+                .addComponent(btnAbrir)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addComponent(btnCerrar)
+                .addGap(30, 30, 30))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(168, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(139, 139, 139))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(237, 237, 237))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblEstadoAlarma)
+                        .addGap(219, 219, 219))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblEstadoBloqueo)
+                    .addComponent(lblEstadoPuerta))
+                .addGap(12, 12, 12)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnBloquear)
+                    .addComponent(btnDesbloquear)
+                    .addComponent(btnAbrir)
+                    .addComponent(btnCerrar))
+                .addGap(58, 58, 58)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblEstadoAlarma)
+                .addContainerGap(159, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAlarmaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAlarmaActionPerformed
-        if(simulador.getVehiculo().getAlarma().isActiva()){
-            simulador.getVehiculo().getAlarma().desactivarAlarma();
-            btnAlarma.setText("Encender alarma");
-        }else{
-            simulador.getVehiculo().getAlarma().activarAlarma();
-            btnAlarma.setText("Apagar alarma");
-        }
-    }//GEN-LAST:event_btnAlarmaActionPerformed
+    private void btnBloquearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBloquearActionPerformed
+       simulador.getVehiculo().getPuertas().bloquear();
+       lblEstadoBloqueo.setText("Estado: Bloqueada");
+    }//GEN-LAST:event_btnBloquearActionPerformed
 
-    private void btnCierreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCierreActionPerformed
-        if( simulador.getVehiculo().getPuertas().isBloqueada()){
-            simulador.getVehiculo().getPuertas().abrir();
-            btnCierre.setText("Cerrar cierre");
-        }else{
-            simulador.getVehiculo().getPuertas().bloquear();
-            btnCierre.setText("Abrir cierre");
-        }
-    }//GEN-LAST:event_btnCierreActionPerformed
+    private void btnDesbloquearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDesbloquearActionPerformed
+       simulador.getVehiculo().getPuertas().desbloquear();
+       lblEstadoBloqueo.setText("Estado: Desbloqueada");
+    }//GEN-LAST:event_btnDesbloquearActionPerformed
+
+    private void btnAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAbrirActionPerformed
+    if (!simulador.getVehiculo().getPuertas().isBloqueada()) {
+        simulador.getVehiculo().getPuertas().abrir();
+        lblEstadoPuerta.setText("Estado: Abierta");
+        simulador.getVehiculo().getAlarma().activarAlarma();
+        lblEstadoAlarma.setText("Estado: Activada");
+    } else {
+        lblEstadoPuerta.setText("No se puede abrir: Puerta bloqueada");
+    }
+    }//GEN-LAST:event_btnAbrirActionPerformed
+
+    private void btnCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCerrarActionPerformed
+    simulador.getVehiculo().getPuertas().cerrar();
+    lblEstadoPuerta.setText("Estado: Cerrada");
+    simulador.getVehiculo().getAlarma().desactivarAlarma();
+    lblEstadoAlarma.setText("Estado: Inactiva");
+    }//GEN-LAST:event_btnCerrarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAlarma;
-    private javax.swing.JButton btnCierre;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton btnAbrir;
+    private javax.swing.JButton btnBloquear;
+    private javax.swing.JButton btnCerrar;
+    private javax.swing.JButton btnDesbloquear;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel lblEstadoAlarma;
+    private javax.swing.JLabel lblEstadoBloqueo;
+    private javax.swing.JLabel lblEstadoPuerta;
     // End of variables declaration//GEN-END:variables
 }
